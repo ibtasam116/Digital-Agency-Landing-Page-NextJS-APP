6c524f22c2eb0d11feb680e345f500fbfdc8251b
ibtasam116 <ibtasam116@gmail.com>
2023-09-12 11:59:58 +0500

    Final

--- app/(components)/navbar/navbar.js
+++ app/(components)/navbar/navbar.js
@@ -11,13 +11,13 @@ export default function Navbar() {
                     <Link href="/" class="flex items-center">
                         <span class="uppercase text-xl sm:text-3xl font-bold hover:text-sky-500 transition-all delay-100">Logo.</span>
                     </Link>
-                    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:text-sky-500 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
                     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                        <ul class="font-medium flex flex-col md:items-center p-4 md:p-0 rounded-lg bg-[#122E99] md:flex-row mt-1  md:space-x-3 space-y-3 md:space-y-0 md:mt-0 md:border-0 md:bg-[#122E99] dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul class="text-sm flex flex-col md:items-center p-4 md:p-0 rounded-lg bg-[#122E99] md:flex-row md:space-x-4 space-y-3 md:space-y-0 md:mt-0 md:border-0 md:bg-[#122E99] dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
                                 <Link href="/" className="hover:text-sky-500 hover:underline transition-all delay-100">Home</Link>
                             </li>
@@ -37,7 +37,7 @@ export default function Navbar() {
                                 <Link href="/blog" className="hover:text-sky-500 hover:underline transition-all delay-100">Blog</Link>
                             </li>
                             <li>
-                                <Link href="/contact" className="hover:text-sky-500 hover:underline transition-all delay-100">Contact</Link>
+                                <Link href="/contact" className="hover:text-sky-500 hover:underline transition-all delay-100 pe-0 lg:pe-7">Contact</Link>
                             </li>
                             <li>
                                 <form class="sm:hidden lg:inline-block">
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,9 +1,11 @@
 import Header from "../(components)/header/header";
+import Clients from "../home/home_sections/clients/clients";
 
 export default function Page() {
     return (
         <>
-            <Header header_title="Contact"/>
+            <Header header_title="Contact" />
+            <Clients />
         </>
     )
 }
