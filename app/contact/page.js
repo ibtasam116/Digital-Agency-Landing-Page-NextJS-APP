bf428742e07de507a4ec2967272330a22c0b846b
ibtasam116 <ibtasam116@gmail.com>
2023-09-12 12:58:51 +0500

    Finaly Complete this Project

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,10 +1,12 @@
 import Header from "../(components)/header/header";
 import Clients from "../home/home_sections/clients/clients";
+import Contact_Us_Cards from "./(components)/Contact_Us_Cards/Contact_Us_Cards";
 
 export default function Page() {
     return (
         <>
             <Header header_title="Contact" />
+            <Contact_Us_Cards />
             <Clients />
         </>
     )
